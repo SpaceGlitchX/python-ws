284a39fa494b31e8dbba16e0d009f5bb4b5e88b0
Madison Sevcik <madisonsevcik@gmail.com>
2026-06-27 14:42:19 -0400

    ms

---
+++ lab2/lcd-display.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+from RPLCD.gpio import CharLCD
+
+""" LCD DISPLAY
+    - This script initializes a 16x2 LCD display to confirm that the display is working properly.
+    - Pin wiring: VSS -> GND, VDD -> 5V, V0 -> POTENTIOMETER, RS=26, RW=GND, D4=21, D5=20, D6=16, D7=12, A=5V, K=GND
+    """
+
+lcd = CharLCD(numbering_mode=GPIO.BCM, cols=16, rows=2, pin_rs=26, pin_e=19, pins_data=[21, 20, 16, 12])
+
+lcd.cursor_pos = (0, 0)
+lcd.write_string(f"LCD Display Test")  
