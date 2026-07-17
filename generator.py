a23b41db59de5bece94a62651e3689ddc9a7971d
Madison <madisonsevcik@gmail.com>
2026-07-17 13:00:59 -0400

    MS

--- generator.py
+++ generator.py
@@ -3,6 +3,7 @@ import lgpio
 
 GPIO_PIN = 22
 FREQ = 200  # Frequency in Hz
+DUTY_CYCLE = 50  # Duty cycle in percentage
 
 h = lgpio.gpiochip_open(0)  # Open the GPIO chip
 lgpio.gpio_claim_output(h, GPIO_PIN)  # Claim the GPIO pin for output
@@ -11,7 +12,7 @@ print(f"Generating a {FREQ} Hz square wave on GPIO pin {GPIO_PIN}...")
 print("Press Ctrl+C to stop.")
 
 try:
-    lgpio.tx_pwm(h, GPIO_PIN, FREQ, 50)  # Start generating the square wave
+    lgpio.tx_pwm(h, GPIO_PIN, FREQ, DUTY_CYCLE)  # Start generating the square wave
     while True:
         time.sleep(1)  # Keep the program running
 
@@ -19,7 +20,7 @@ except KeyboardInterrupt:
     print("\nStopping signal...")
 
 finally:
-    lgpio.tx_pwm(h, GPIO_PIN, 0, 50)  # Stop generating the square wave
+    lgpio.tx_pwm(h, GPIO_PIN, 0, DUTY_CYCLE)  # Stop generating the square wave
     lgpio.gpio_free(h, GPIO_PIN)  # Close the GPIO chip
     lgpio.gpiochip_close(h)  # Close the GPIO chip
     print("Signal generation stopped.")
