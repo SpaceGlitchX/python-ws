1a8e3fa9e297c79bdbc99faf4cffcaf03186a843
Madison <madisonsevcik@gmail.com>
2026-08-18 19:39:37 -0400

    ma

---
+++ lab8/KY-040.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+
+# Constants
+CLK = 17
+SW = 22
+DT = 27
+DURATION = 10
+
+# Setup GPIOs
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(CLK, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(SW, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(DT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+# Event-driven call-back
+def signal_event(channel):
+    
+    if GPIO.input(CLK):
+        print("CLK line triggered.")
+    else:
+        print("SW line triggered.")
+        
+    # Output logic states of SW, CLK, DT
+    print(f"SW = {SW}\nCLK = {CLK}\nDT = {DT}\n")
+
+# Define interrupt-driven events
+GPIO.add_event_detect(CLK, GPIO.FALLING, callback = signal_event, bouncetime = 300)
+GPIO.add_event_detect(SW, GPIO.FALLING, callback = signal_event, bouncetime = 300)
+
+start = time.time()
+d = time.time() - start
+while d <= DURATION:
+    d = time.time() - start
