e18b568db0dfd507c6b1ee2d4a25caa7dc5f6769
Madison <madisonsevcik@gmail.com>
2026-08-18 21:09:48 -0400

    ms

--- lab8/KY-040.py
+++ lab8/KY-040.py
@@ -3,9 +3,14 @@ import time
 
 # Constants
 CLK = 17
-SW = 22
-DT = 27
-DURATION = 10
+SW = 27
+DT = 18
+
+# Global position
+Pos = 0
+
+# Encoder resolution
+DEGREES_PER_STEP = 360 / 20
 
 # Setup GPIOs
 GPIO.setmode(GPIO.BCM)
@@ -16,20 +21,54 @@ GPIO.setup(DT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Event-driven call-back
 def signal_event(channel):
+    global Pos
+    # Read pin states
+    clk_state = GPIO.input(CLK)
+    dt_state = GPIO.input(DT)
+    sw_state = GPIO.input(SW)
     
-    if GPIO.input(CLK):
+    # CLK triggered = encoder rotated
+    if channel == CLK:
         print("CLK line triggered.")
-    else:
-        print("SW line triggered.")
         
-    # Output logic states of SW, CLK, DT
-    print(f"SW = {SW}\nCLK = {CLK}\nDT = {DT}\n")
-
+        # Determine direction
+        if dt_state == GPIO.LOW:
+            # Counter-clockwise
+            Pos += DEGREES_PER_STEP
+            
+        else:
+            # Clockwise
+            Pos -= DEGREES_PER_STEP
+    elif channel == SW:
+        
+        print("SW line trigger.")
+        # Reset position
+        
+    # Print logic states
+    print("CLK = ", clk_state)
+    print("DT = ", dt_state)
+    print("SW = ", sw_state)
+    
+    # Print position
+    print("Position = ", Pos, "degrees")
+    
 # Define interrupt-driven events
 GPIO.add_event_detect(CLK, GPIO.FALLING, callback = signal_event, bouncetime = 300)
 GPIO.add_event_detect(SW, GPIO.FALLING, callback = signal_event, bouncetime = 300)
 
-start = time.time()
-d = time.time() - start
-while d <= DURATION:
-    d = time.time() - start
+
+# Main loop
+try:
+    print("Encoder running...")
+    print("Turn encoder to change position.")
+    print("Press button to reset position.")
+    print("Press Ctrl+C to stop.")
+
+    while True:
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("\nProgram stopped.")
+
+finally:
+    GPIO.cleanup()
